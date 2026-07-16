c2c1289de93e58a2dc8f6ab8d4854a1929ca328e
Matheus de Sousa dos Santos <matheusmtsm75@gmail.com>
2026-07-16 11:28:28 -0300

    commit inicial

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
+
+@Component({
+  selector: 'app-root',
+  imports: [ListaProdutos],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('e-commerce-aluno');
+}
