a98f7f2bdeffc10f9191443bc92f62ef5c010403
Mariana_Jorry <marianajorry.1@gmail.com>
2026-07-21 16:07:30 -0300

    aula-11: carrinho com signals

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -13,48 +13,59 @@ import { UpperCasePipe } from '@angular/common';
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
-  //!Lista com dados
+
+  //! SIGNALS
+
  produtos = signal ([
-    { 
-      nome: 'Teclado Gamer', 
-      preco:149.00
-    },
-    {
-      nome: 'Mouse Gamer', 
-      preco:299.99
-    },
-    {
-      nome: 'Monitor Gamer', 
-      preco:1599.99
-    },
-    {
-      nome: 'Desktop Gamer', 
-      preco:4999.99
-    },
-    {
-      nome: 'Headset Gamer', 
-      preco:699.99
-    }
+    {nome: 'Teclado Gamer',preco:149.00},
+    {nome: 'Mouse Gamer',preco:299.99},
+    {nome: 'Monitor Gamer',preco:1599.99},
+    {nome: 'Desktop Gamer',preco:4999.99},
+    {nome: 'Headset Gamer',preco:699.99}
   ]);
-//!Função apara exibir produtos selecionados pelo usuario no console
-  exibirProduto (nome: string){
+
+//! função que contabiliza a quantidade de produtos na lista
+totalProdutos = computed(() => this.produtos().length);
+
+
+//! função que calcula o valor total do metodo comput()
+
+valorTotal = computed(()=> 
+{return this.produtos()
+.reduce((total, item) => total + item.preco,0)
+});
+
+constructor(){
+ effect(() => {
+  console.log('lista de Produtos Alterados: ', this.produtos());
+});
+
+effect(() => {
+  console.log( ' Valor Total Atualizado: ', this.valorTotal());
+});
+
+effect(() => {
+  if  (typeof document !== 'undefined'){
+    document.title = `(${this.totalProdutos()}) - Lojinha da Jorryzinha`;
+    }
+ });
+}
+
+exibirProduto (nome: string){
     console.log ('Produto Selecionado: ', nome);
     this.produtoSelecionado.set(nome);
   }
-//!função que adiciona produto usando metodo uptade()
-  adicionarProdutos(){
-    this.produtos.update(listaAtual => [...listaAtual,
+
+  // Update() modifica baseado no estado atual
+adicionarProdutos(){
+    this.produtos.update(listaAtual => [
+      ...listaAtual,
       {nome: 'Playstation 5', preco:3000}
     ]);
-  }
-//! função que contabiliza a quantidade de produtos na lista
-totalProdutos = computed(() => this.produtos().length);
-//! função que calcula o valor total do metodo comput()
-valorTotal = computed(()=> 
-{return this.produtos().reduce((total, item) =>
-  total + item.preco,0)});
-  //!função para substitui a lista atual usando metodo set
-  substituirProdutos() {
+}
+
+// set() substitui completamente
+substituirProdutos() {
 this.produtos.set([
     {nome: 'Teclado', preco: 50},
     {nome: 'Mouse', preco: 15},
@@ -62,22 +73,20 @@ this.produtos.set([
     {nome: 'Desktop', preco: 1500},
     {nome: 'Headset', preco: 30},
    ]);
-  }
-  //! Metodo para monitorar alterações em tempo real usando effect()
-  constructor(){
-effect(() => {
-  console.log('lista de Produtos Alterados: ', this.produtos());
-});
-effect(() => {
-console.log( ' Valor Total Atualizado: ', this.valorTotal());
-});
-effect(() => {
-  if  (typeof document !== 'undefined'){
-    document.title = `(${this.totalProdutos()}) - Lojinha da Jorryzinha`;
-  }
-});
-//! Metodo para criar um estado de selção com signal string | null
-
-  }
-  produtoSelecionado = signal <String | null>(null);
 }
+
+  produtoSelecionado = signal <string | null>(null);
+  carrinho = signal<{ nome: string; preco: number } []>([]);
+  
+adicionarAoCarrinho(produto: { nome: string; preco: number }) {
+   this.carrinho.update(listaAtual => [
+    ...listaAtual,
+    produto
+  ]);
+}
+  quantidadeCarrinho = computed(() => this.carrinho().length);
+  totalCarrinho = computed(() => {
+return this.carrinho().reduce((total, item) =>
+  total + item.preco, 0);
+  });
+  }
