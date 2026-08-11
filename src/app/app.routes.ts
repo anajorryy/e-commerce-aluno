a30de7b19166582658ac50462cd2b0f9d51557f1
Mariana_Jorry <Marianajorry.1@gmail.com>
2026-08-11 20:03:24 -0300

    feat:Rota do Checkout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -21,6 +21,9 @@ export const routes: Routes = [
         import('./features/carrinho/carrinho/carrinho')
     .then((m) => m.Carrinho),
  },
+ {path: 'checkout',
+   loadComponent:() => import('./features/checkout/checkout/checkout').then((m) => m.Checkout),
+ },
  {
     path:'**',
     redirectTo:'',
