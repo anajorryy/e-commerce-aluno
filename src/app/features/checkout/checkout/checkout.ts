da824f26037c501f4e13e414a56ccedc6ab2e1a8
Mariana_Jorry <Marianajorry.1@gmail.com>
2026-08-11 21:25:47 -0300

    feat(checkout): adiciona lógica de validação e finalização da compra

--- src/app/features/checkout/checkout/checkout.ts
+++ src/app/features/checkout/checkout/checkout.ts
@@ -1,21 +1,46 @@
 import { Component, inject } from '@angular/core';
-import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { ReactiveFormsModule,FormGroup,FormControl,} from '@angular/forms';
 import { CarrinhoService } from '../../../core/services/carrinho.service';
+import { Validators, AbstractControl, ValidationErrors } from '@angular/forms';
+
 @Component({
-selector: 'app-checkout',
-imports: [ReactiveFormsModule],
-templateUrl: './checkout.html',
-styleUrl: './checkout.css',
+  selector: 'app-checkout',
+  imports: [ReactiveFormsModule],
+  templateUrl: './checkout.html',
+  styleUrl: './checkout.css',
 })
 export class Checkout {
-carrinhoService = inject(CarrinhoService);
+  carrinhoService = inject(CarrinhoService);
+
 formulario = new FormGroup({
-nome: new FormControl(''),
-email: new FormControl(''),
-endereco: new FormControl(''),
+ nome: new FormControl('',[Validators.required, Validators.minLength(3), nomeSemNumeros]),
+ email: new FormControl('',[Validators.required, Validators.email]),
+ endereco: new FormControl('',[Validators.required, Validators.minLength(5)]),
 });
+
 finalizar() {
-console.log('Dados do formulário:', this.formulario.value);
-console.log('Itens do carrinho:', this.carrinhoService.itens());
+
+  if(this.formulario.invalid){
+    console.log('Formulário Inválido');
+    return;
+  }
+
+
+  const dados = this.formulario.value
+  const itens = this.carrinhoService.itens();
+
+
+  
+ console.log('Dados do formulário:', this.formulario.value);
+ console.log('Itens do carrinho:', this.carrinhoService.itens());
+ }
 }
+
+function nomeSemNumeros (control:AbstractControl): ValidationErrors | null {
+  const valor = control.value;
+  if(!valor) return null;
+  if(/\d/.test(valor)){ //detecta e bloqueia numeros no sistema
+    return {numeroInvalido:true};
+  } 
+  return null;
 }
